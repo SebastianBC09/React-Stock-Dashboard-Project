0bbd63b22e086ad39782bc2a32d657460520b673
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-04-25 02:08:21 -0500

    Other charts section created, styles added & finished. ChartA section also created & styles added too.

--- bc-stock-exchange/src/App.js
+++ bc-stock-exchange/src/App.js
@@ -8,26 +8,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    /*<div className="App">
-       <header className="App-header">
-         <img src={logo} className="App-logo" alt="logo" />
-         <p>
-           Edit <code>src/App.js</code> and save to reload.
-         </p>
-         <a
-           className="App-link"
-           href="https://reactjs.org"
-           target="_blank"
-           rel="noopener noreferrer"
-         >
-           Learn React
-         </a>
-       </header>
-     </div>,*/
     <section className="container-fluid">
       <div className="row">
         <div className="row">
-          <div className="col-md-1">
+          <div className="col-md-1 ">
             <aside className="ctn-sidebar">
               <ul className="sidebar-links">
                 <li className="sd-logo">
@@ -50,7 +34,7 @@ function App() {
                 <section className="ctn-charts glassmorphism-effect">
                 </section>
                 <div className="row">
-                  <div className="col-md-12">
+                  <div className="col-md-11">
                     <section className="ctn-others-charts">
                       <section className="ctn-chartA"></section>
                       <section className="ctn-chartB"></section>
