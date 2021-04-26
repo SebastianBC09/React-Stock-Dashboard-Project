f5228cb5e589ddfa90b5809f1ab7168f24e29560
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-04-25 20:54:23 -0500

    Lightly changes made to different files. Indentation & organization.

--- bc-stock-exchange/src/App.js
+++ bc-stock-exchange/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faApple } from '@fortawesome/free-brands-svg-icons'
 import { faAmazon } from '@fortawesome/free-brands-svg-icons'
